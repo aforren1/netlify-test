fix scaling woes

scratch_seq.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft
 
 
-def generate_probs(cycles=1, rng=default_rng()):
-    freqs = np.array([2, 3, 5, 11])
-    trials_per_cycle = np.prod(freqs)
-    base_amp = 0.889 # max amp is ~1, after summing
-    amps = base_amp / freqs
-    amps = np.array((0.25, 0.25, 0.25, 0.25))
+def generate_probs(trials=200, cycles=1, rng=default_rng()):
+    freqs = np.array([2, 3, 5, 11, 17])
+    #trials_per_cycle = np.prod(freqs)
+    trials_per_cycle = trials
+    #base_amp = 0.889 # max amp is ~1, after summing
+    #amps = base_amp / freqs
+    amps = np.array([.2]*5)
     phases = rng.uniform(0, 2*np.pi, freqs.size)
     t = np.arange(0, cycles, 1 / trials_per_cycle)
     out = np.zeros(trials_per_cycle * cycles)
@@ -22,10 +23,11 @@ def generate_probs(cycles=1, rng=default_rng()):
 
 cycles = 1
 n_sims = 1
-vals = np.empty((n_sims, 330 * cycles))
+trials = 200
+vals = np.empty((n_sims, trials * cycles))
 rng = default_rng()
 for i in range(vals.shape[0]):
-    vals[i, :] = generate_probs(cycles, rng)
+    vals[i, :] = generate_probs(trials, cycles, rng)
 
 out = rng.binomial(1, vals)
 plt.plot(vals.T, label='P(Reward)')
@@ -36,8 +38,8 @@ plt.ylabel('Reward')
 plt.title('Simulated reward schedule & potential outcome')
 plt.show()
 
-n = 330 * cycles
-t = 1 / 330
+n = trials * cycles
+t = 1 / trials
 x = np.arange(0, cycles, 1/n)
 y = vals[0,:] - np.mean(vals[0,:])
 yf = fft(y)